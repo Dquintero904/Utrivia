7d949d1a9cb8289cff5cd37b4110ce700403da98
Daniela <dquinteroa3@ucentral.edu.co>
2024-09-27 22:24:38 -0300

    (NUEVO:) Subiendo al estructura del proyecto

---
+++ src/main/java/co/ucentral/Utrivia/UtriviaApplication.java
@@ -0,0 +1,13 @@
+package co.ucentral.Utrivia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtriviaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtriviaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/ucentral/Utrivia/UtriviaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.ucentral.Utrivia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtriviaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
